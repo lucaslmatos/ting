8da9364758c1522746f83a2e1ebf5536bb03e777
Lucas Matos <lucaslmatos18@gmail.com>
2023-08-28 15:49:29 -0300

    Req 02 - txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file, 'r') as file: 
+            # O with garante que o arquivo será fehcado apos uso
+            return file.read().split("\n")
+    except FileNotFoundError:
+        # O bloco try tentará encontrar o arquivo no caminho especificado,
+        # caso haja algum erro irá retornar a mensagem de erro
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
