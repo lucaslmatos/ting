b5bbadaa3b9b968e6284fc45f7c77a2bc00996c0
Lucas Matos <lucaslmatos18@gmail.com>
2023-08-28 16:30:56 -0300

    Req 03 - file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_name = path_file
+    already_processed = False
+    for item in instance.queue:
+        if item["nome_do_arquivo"] == file_name:
+            already_processed = True
+            break
+    if not already_processed:
+        lines = txt_importer(path_file)
+
+        file_info = {
+            "nome_do_arquivo": file_name,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines
+        }
+        instance.enqueue(file_info)
+        print(file_info)
 
 
 def remove(instance):
