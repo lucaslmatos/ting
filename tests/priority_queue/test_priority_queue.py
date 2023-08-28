474d13e6398761e58e5452cbc97db12c01b9589d
Lucas Matos <lucaslmatos18@gmail.com>
2023-08-28 19:21:08 -0300

    Req 06 - priority_queue tests

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,25 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+
+    items = [{"nome_do_arquivo": "item_1.txt",
+              "qtd_linhas": 8},
+             {"nome_do_arquivo": "item_2.txt",
+              "qtd_linhas": 2}]
+
+    for item in items:
+        queue.enqueue(item)
+
+    assert len(queue) == 2
+
+    assert queue.search(0) == items[1]
+    assert queue.search(1) == items[0]
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(4)
+
+    assert queue.dequeue() == items[1]
+    assert queue.dequeue() == items[0]
