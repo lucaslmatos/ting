03cbbcf4aa8f2d4f99bfb31b9f443ac6042d5b34
Lucas Matos <lucaslmatos18@gmail.com>
2023-08-28 18:21:06 -0300

    Req 07 - exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,17 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for item in instance.queue:
+        ocorrencias = []
+        for j, linha in enumerate(item['linhas_do_arquivo']):
+            if word.upper() in linha.upper():
+                ocorrencias.append({"linha": j + 1})
+        if ocorrencias != []:
+            result.append({
+                    "palavra": word,
+                    "arquivo": item['nome_do_arquivo'],
+                    "ocorrencias": ocorrencias
+                })
+    return result
 
 
 def search_by_word(word, instance):
